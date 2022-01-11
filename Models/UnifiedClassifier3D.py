973c15e051677465e47c68c606bdef33d0c3f3fe
Paolo Zaffino <p.zaffino@unicz.it>
2022-01-11 20:29:41 +0100

    Custom 3D unet implementation

--- Models/UnifiedClassifier3D.py
+++ Models/UnifiedClassifier3D.py
@@ -10,7 +10,8 @@ from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from pytorch_lightning import LightningDataModule, LightningModule, Trainer,seed_everything
 import sys
 import torchio as tio
-from Models.unet3d import UNet3D
+#from Models.unet3d import UNet3D
+from Models.custom_3DUnet import UNet
 import sklearn
 from pytorch_lightning import loggers as pl_loggers
 import torchmetrics
@@ -20,8 +21,8 @@ class UnifiedClassifier3D(LightningModule):
     def __init__(self, n_classes = 1, wf=5, depth=3):
         super().__init__()
 
-        self.anatomy_unet_model = torch.nn.Sequential(UNet3D(in_channels=1, n_classes = n_classes, depth=depth,wf=wf).encoder, torch.nn.Flatten())
-        self.dose_unet_model = torch.nn.Sequential(UNet3D(in_channels=1, n_classes = n_classes, depth=depth,wf=wf).encoder, torch.nn.Flatten())
+        self.anatomy_unet_model = torch.nn.Sequential(UNet(), torch.nn.Flatten())
+        self.dose_unet_model = torch.nn.Sequential(UNet(), torch.nn.Flatten())
 
         self.classifier = torch.nn.Sequential(
             torch.nn.LazyLinear(128),
@@ -34,20 +35,20 @@ class UnifiedClassifier3D(LightningModule):
         anatomy_features = self.anatomy_unet_model(anatomy)
         dose_features = self.dose_unet_model(dose)
         total_features = torch.cat([anatomy_features, dose_features], dim=1)
-        print(total_features)
+        #print(total_features)
         return self.classifier(total_features)
 
     def training_step(self, batch, batch_idx):
         datadict, label = batch
         prediction  = self.forward(datadict["Anatomy"], datadict["Dose"])
         loss = self.loss_fcn(prediction.squeeze(), label)
-        return {"loss":loss,"prediction":prediction.squeeze(),"label":label}
+        return {"loss":loss,"prediction":prediction.squeeze().detach(),"label":label}
 
     def validation_step(self, batch, batch_idx):
         datadict, label = batch
         prediction  = self.forward(datadict["Anatomy"], datadict["Dose"])
         loss = self.loss_fcn(prediction.squeeze(), label)
-        return {"loss":loss,"prediction":prediction.squeeze(),"label":label}
+        return {"loss":loss,"prediction":prediction.squeeze().detach(),"label":label}
 
     def configure_optimizers(self):
         lr=1e-6
