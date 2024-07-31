bug fixes, added normalization and one-hot-encoding

Classification.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision
 from torch import nn
+import torch.distributed as dist
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping, LearningRateMonitor
 from pytorch_lightning import LightningDataModule, LightningModule, Trainer, seed_everything
 import sys, os
@@ -8,6 +9,7 @@ import monai
 from pytorch_lightning.strategies import DDPStrategy
 import numpy as np
 import pandas as pd
+from pathlib import Path
 
 ## Module - Dataloaders
 from DataGenerator.DataGenerator import DataModule
@@ -16,6 +18,7 @@ from Models.Linear import Linear
 from Models.MixModel import MixModel
 from monai.transforms import EnsureChannelFirstd, ScaleIntensityd, ResampleToMatchd, BoundingRectd
 from Utils.DataExtraction import create_subject_list
+from Utils.Transformations import StandardScalerd
 
 ## Main
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
@@ -24,6 +27,9 @@ from Utils.PredictionReports import PredictionReports
 from pathlib import Path
 from pytorch_lightning.loggers import TensorBoardLogger
 
+
+def is_rank_zero():
+    return not dist.is_initialized() or dist.get_rank() == 0
 
 def threshold_at_one(x):
     return x > 2.1
@@ -36,25 +42,48 @@ def load_config():
 
 def transform_pipeline(config):
     img_keys = [k for k in config['MODALITY'].keys() if config['MODALITY'][k]]
+    records_keys = ['records'] if config['RECORDS']['records'] else []
 
-    if config['MODALITY'].values():
-        train_transform = torchvision.transforms.Compose([
-            EnsureChannelFirstd(keys=img_keys + ['RTSTRUCT'] if 'RTSTRUCT' in config['MODALITY'].keys() else img_keys),
-            monai.transforms.CropForegroundd(keys=img_keys, source_key='RTSTRUCT', select_fn=threshold_at_one),
-            monai.transforms.Resized(keys=img_keys, spatial_size=config['DATA']['dim']),
-            monai.transforms.RandAffined(keys=img_keys),
-            monai.transforms.RandHistogramShiftd(keys=img_keys),
-            monai.transforms.RandAdjustContrastd(keys=img_keys),
-            monai.transforms.RandGaussianNoised(keys=img_keys),
-            monai.transforms.ScaleIntensityd(keys=list(set(img_keys).difference(set(['RTDOSE'])))),
-        ])
+    if len(records_keys) > 0 or len(img_keys) > 0:
+        train_transform = []
+        val_transform = []
 
-        val_transform = torchvision.transforms.Compose([
-            EnsureChannelFirstd(keys=img_keys + ['RTSTRUCT'] if 'RTSTRUCT' in config['MODALITY'].keys() else img_keys),
-            monai.transforms.CropForegroundd(keys=img_keys, source_key='RTSTRUCT', select_fn=threshold_at_one),
-            monai.transforms.Resized(keys=img_keys, spatial_size=config['DATA']['dim']),
-            monai.transforms.ScaleIntensityd(list(set(img_keys).difference(set(['RTDOSE'])))),
-        ])
+        if len(records_keys) > 0:
+            if 'continuous_cols' not in config['DATA'].keys():
+                non_continuous = [config['DATA']['target'], config['DATA']['censor_label'],
+                                  config['DATA']['subject_label']]
+                config['DATA']['continuous_cols'] = [col for col in config['DATA']['clinical_cols']
+                                                     if col not in non_continuous]
+            train_transform += [
+                StandardScalerd(keys=records_keys, continuous_variables=config['DATA']['continuous_cols']),]
+            val_transform += [
+                StandardScalerd(keys=records_keys, continuous_variables=config['DATA']['continuous_cols']),]
+
+        if len(img_keys) > 0:
+            condition = (('RTSTRUCT' not in config['MODALITY'].keys()) or (not config['MODALITY']['RTSTRUCT']) and
+                         (config['MODALITY']['CT']) and ('CT' in config['MODALITY'].keys()))
+            train_transform = [
+                # EnsureChannelFirstd(keys=img_keys + ['RTSTRUCT'] if 'RTSTRUCT' in config['MODALITY'].keys() else img_keys),
+                EnsureChannelFirstd(keys=img_keys + ['RTSTRUCT'] if condition else img_keys),
+                monai.transforms.CropForegroundd(keys=img_keys, source_key='RTSTRUCT', select_fn=threshold_at_one),
+                monai.transforms.Resized(keys=img_keys, spatial_size=config['DATA']['dim']),
+                monai.transforms.RandAffined(keys=img_keys),
+                monai.transforms.RandHistogramShiftd(keys=img_keys),
+                monai.transforms.RandAdjustContrastd(keys=img_keys),
+                monai.transforms.RandGaussianNoised(keys=img_keys),
+                monai.transforms.ScaleIntensityd(keys=list(set(img_keys).difference(set(['RTDOSE'])))),
+            ]
+
+            val_transform = [
+                # EnsureChannelFirstd(keys=img_keys + ['RTSTRUCT'] if 'RTSTRUCT' in config['MODALITY'].keys() else img_keys),
+                EnsureChannelFirstd(keys=img_keys + ['RTSTRUCT'] if condition else img_keys),
+                monai.transforms.CropForegroundd(keys=img_keys, source_key='RTSTRUCT', select_fn=threshold_at_one),
+                monai.transforms.Resized(keys=img_keys, spatial_size=config['DATA']['dim']),
+                monai.transforms.ScaleIntensityd(list(set(img_keys).difference(set(['RTDOSE'])))),
+            ]
+
+        train_transform = torchvision.transforms.Compose(train_transform)
+        val_transform = torchvision.transforms.Compose(val_transform)
     else:
         train_transform = None
         val_transform = None
@@ -74,8 +103,9 @@ def build_model(config, clinical_cols):
                 if 'RTSTRUCT' in module_dict.keys():
                     module_dict.pop('RTSTRUCT')
 
-    if 'Records' in config.keys():
-        module_dict['Records'] = Linear(in_feat=len(clinical_cols), out_feat=42)
+    if 'RECORDS' in config.keys() and config['RECORDS']['records']:
+        module_dict['records'] = Linear(config, in_feat=len(clinical_cols),
+                                        out_feat=config['MODEL']['linear_out'])
 
     return module_dict
 
@@ -100,7 +130,7 @@ def main(config, rd):
     seed_everything(rd, workers=True)
     model_name = 'banana'
     SubjectList = create_subject_list(config)
-    print(SubjectList)
+    SubjectList.to_csv(Path(config['DATA']['log_folder'])/'data_table.csv')
     clinical_cols = config['DATA']['clinical_cols']
     logger = get_logger(config, model_name)
     callbacks = get_callbacks()
@@ -120,45 +150,49 @@ def main(config, rd):
 
     trainer = Trainer(
         accelerator="gpu",
-        # devices=[0, 1, 2, 3],
-        devices=[0],
+        devices=config['MODEL']['devices'],
         strategy=DDPStrategy(find_unused_parameters=True),
-        max_epochs=40,
+        max_epochs=config['MODEL']['max_epochs'],
         logger=logger,
-        log_every_n_steps=5,
+        log_every_n_steps=1,
         callbacks=callbacks,
     )
 
+    # Ensure the directory exists only on rank 0
+    if is_rank_zero():
+        if not Path(logger.log_dir).exists():
+            Path(logger.log_dir).mkdir(parents=True)
+        with open(logger.log_dir + '/patient_list.ini', 'w+') as patient_list_file:
+            patient_list_file.write('random_seed:\n')
+            patient_list_file.write(str(rd))
+            patient_list_file.write('\n')
+            patient_list_file.write('train_patient_list:\n')
+            patient_list_file.write('. '.join(list(dataloader.train_list[config['DATA']['subject_label']])))
+            patient_list_file.write('\n')
+            patient_list_file.write('val_patient_list:\n')
+            patient_list_file.write('. '.join(list(dataloader.val_list[config['DATA']['subject_label']])))
+            patient_list_file.write('\n')
+
+        with open(logger.root_dir + "/Config.ini", "w+") as config_file:
+            toml.dump(config, config_file)
+            config_file.write("Train transform:\n")
+            config_file.write(str(train_transform))
+            config_file.write("Val/Test transform:\n")
+            config_file.write(str(val_transform))
+
     trainer.fit(model, dataloader)
-
-    with open(logger.log_dir + '/patient_list.ini', 'w+') as patient_list_file:
-        patient_list_file.write('random_seed:\n')
-        patient_list_file.write(str(rd))
-        patient_list_file.write('\n')
-        patient_list_file.write('train_patient_list:\n')
-        patient_list_file.write('. '.join(list(dataloader.train_list['subject_label'])))
-        patient_list_file.write('\n')
-        patient_list_file.write('val_patient_list:\n')
-        patient_list_file.write('. '.join(list(dataloader.val_list['subject_label'])))
-        patient_list_file.write('\n')
-
-    with open(logger.root_dir + "/Config.ini", "w+") as config_file:
-        toml.dump(config, config_file)
-        config_file.write("Train transform:\n")
-        config_file.write(str(train_transform))
-        config_file.write("Val/Test transform:\n")
-        config_file.write(str(val_transform))
 
 
 if __name__ == "__main__":
     config = (load_config()
               if len(sys.argv) > 1 else toml.load("/home/dgs1/Software/Miguel/OutcomePrediction/TestConfiguration.ini"))
-    y = range(5)
-    random_seed_list = []
+    y = range(config['RUN']['bootstrap_n'])
+    if 'random_state' in config['RUN'].keys():
+        np.random.seed(seed=config['RUN']['random_state'])
+    random_seed_list = np.random.randint(10000, size=len(y))
+    print(random_seed_list)
     for i in y:
-        rd = np.random.randint(10000)
-        random_seed_list.append(rd)
-        main(config, rd)
+        main(config, random_seed_list[i])
 
     print(random_seed_list)
 

DataGenerator/DataGenerator.py
@@ -32,7 +32,7 @@ class DataGenerator(torch.utils.data.Dataset):
 
         data = {}
         meta = {}
-        data['slabel']  = self.SubjectList.loc[i, self.config['DATA']['subject_label']]
+        data['slabel'] = self.SubjectList.loc[i, self.config['DATA']['subject_label']]
         ## Load CT
         if 'CT' in self.keys and self.config['MODALITY']['CT']:
             CTPath = self.SubjectList.loc[i, 'CT_Path']
@@ -56,10 +56,16 @@ class DataGenerator(torch.utils.data.Dataset):
         ## Load Mask
         if 'RTSTRUCT' in self.keys and self.config['MODALITY']['RTSTRUCT']:
             RSPath = self.SubjectList.loc[i, 'RTSTRUCT_Path']
-            data['RTSTRUCT'] = LoadImage(image_only=True)(Path(RSPath, self.config['DATA']['Structs']))
-        else:
+            RS_Path = Path(RSPath, self.config['DATA']['structs'] + '.nii.gz')
+            data['RTSTRUCT'] = LoadImage(image_only=True)(RS_Path)
+            data['RTSTRUCT'][data['RTSTRUCT'] > 0] += 3
+        elif 'CT' in self.keys and self.config['MODALITY']['CT']:
             data['RTSTRUCT'] = deepcopy(data['CT'])
             data['RTSTRUCT'][:] = 3
+
+        # Add clinical record at the end
+        if 'RECORDS' in self.config.keys() and self.config['RECORDS']['records']:
+            data['records'] = self.SubjectList.loc[i, self.clinical_cols].values.astype('float')
 
         if self.transform: data = self.transform(data)
 
@@ -72,11 +78,7 @@ class DataGenerator(torch.utils.data.Dataset):
                 data['RTDOSE'] = np.concatenate([data[key] for key in ['RTDOSE', 'RTSTRUCT']], axis=0)
                 data.pop('RTSTRUCT')
             elif 'RTSTRUCT' in data.keys():
-                data.pop('RTSTRUCT')  ## No need for mask in single-channel multi-branch
-
-        ## Add clinical record at the end
-        if 'Records' in self.config.keys():
-            data['Records'] = self.SubjectList.loc[i, self.clinical_cols].values.astype('float')
+                data.pop('RTSTRUCT')  # No need for mask in single-channel multi-branch
 
         if self.inference:
             return data
@@ -92,7 +94,9 @@ class DataGenerator(torch.utils.data.Dataset):
                 return data, censor_status, label
             else:
                 return data, label
-### DataLoader
+
+
+# DataLoader
 class DataModule(LightningDataModule):
     def __init__(self, SubjectList, config=None, train_transform=None, val_transform=None, train_size=0.7, rd=None,
                  rd_tv=None, num_workers=1, **kwargs):
@@ -100,26 +104,42 @@ class DataModule(LightningDataModule):
         self.batch_size = config['MODEL']['batch_size']
         self.num_workers = 32
 
-        train_list, val_test_list = train_test_split(SubjectList, train_size=train_size,random_state=rd_tv) ## 0.7/0.3
+        train_list, val_test_list = train_test_split(SubjectList, train_size=train_size, random_state=rd_tv)  ## 0.7/0.3
 
-        val_list, test_list = train_test_split(val_test_list, train_size=0.5,random_state=rd_tv)## 0.15/0.15
+        val_list, test_list = train_test_split(val_test_list, train_size=0.5, random_state=rd_tv)  ## 0.15/0.15
+
+        train_transform = self.transform_fit(train_transform, train_list, config)
+        val_transform = self.transform_fit(val_transform, train_list, config)
 
         self.train_list = train_list.reset_index(drop=True)
-        self.val_list   = val_list.reset_index(drop=True)
-        self.test_list  = test_list.reset_index(drop=True)
+        self.val_list = val_list.reset_index(drop=True)
+        self.test_list = test_list.reset_index(drop=True)
 
         self.train_data = DataGenerator(self.train_list, config=config, transform=train_transform, **kwargs)
-        self.val_data   = DataGenerator(self.val_list, config=config, transform=val_transform, **kwargs)
-        self.test_data  = DataGenerator(self.test_list, config=config, transform=val_transform, **kwargs)
+        self.val_data = DataGenerator(self.val_list, config=config, transform=val_transform, **kwargs)
+        self.test_data = DataGenerator(self.test_list, config=config, transform=val_transform, **kwargs)
 
     def train_dataloader(self): return DataLoader(self.train_data, batch_size=self.batch_size,
-                                                  num_workers=self.num_workers, pin_memory=True, shuffle=False)
+                                                  num_workers=self.num_workers, pin_memory=True, shuffle=True)
 
-    def val_dataloader(self):   return DataLoader(self.val_data, batch_size=self.batch_size,
-                                                  num_workers=self.num_workers, pin_memory=True, shuffle=False)
+    def val_dataloader(self): return DataLoader(self.val_data, batch_size=self.batch_size,
+                                                num_workers=self.num_workers, pin_memory=True, shuffle=False)
 
-    def test_dataloader(self):  return DataLoader(self.test_data, batch_size=self.batch_size,
-                                                  num_workers=self.num_workers, pin_memory=True)
+    def test_dataloader(self): return DataLoader(self.test_data, batch_size=self.batch_size,
+                                                 num_workers=self.num_workers, pin_memory=True)
+
+    @staticmethod
+    def transform_fit(transform, data_list, config):
+        if transform is None:
+            return transform
+        if config is not None and config['DATA']['clinical_cols']:
+            cols = config['DATA']['clinical_cols']
+        else:
+            cols = list(data_list.columns)
+        for i, elem in enumerate(transform.transforms):
+            if hasattr(elem, 'fit'):
+                transform.transforms[i].fit(data_list.loc[:, cols])
+        return transform
 
 
 

Models/Classifier.py
@@ -7,6 +7,8 @@ import torchmetrics
 from monai.networks import blocks, nets
 from Models.UnetEncoder import UnetEncoder
 from Models.PretrainedEncoder3D import PretrainedEncoder3D
+
+
 ## Model
 class Classifier(LightningModule):
     def __init__(self, config, module_str):
@@ -30,7 +32,7 @@ class Classifier(LightningModule):
         self.flatten = nn.Sequential(
             # nn.Dropout(0.3),
             # nn.AdaptiveAvgPool3d(output_size=(4, 4, 4)),
-            nn.Dropout(0.3),
+            nn.Dropout(config['MODEL']['dropout_prob']),
             nn.AdaptiveAvgPool3d(output_size=(1, 1, 1)),
             nn.Flatten(),
         )

Models/Linear.py
@@ -15,30 +15,32 @@ import sklearn
 from pytorch_lightning import loggers as pl_loggers
 import torchmetrics
 
+
 ## Model
 class Linear(pl.LightningModule):
-    def __init__(self, in_feat, out_feat):
+    def __init__(self, config, in_feat, out_feat):
         super().__init__()
         self.model = nn.Sequential(
             nn.Linear(in_feat, out_feat),
-            nn.Dropout(0.3),
-            nn.LayerNorm(42),
+            nn.Dropout(config['MODEL']['dropout_prob']),
+            nn.LayerNorm(out_feat),
             nn.ReLU(),
         )
         self.loss_fcn = nn.CrossEntropyLoss()
+        self.config = config
 
     def forward(self, x):
         return self.model(x.float())
 
-    def training_step(self, batch,batch_idx):
-        image,label = batch
-        prediction  = self.forward(image)
+    def training_step(self, batch, batch_idx):
+        image, label = batch
+        prediction = self.forward(image)
         loss = self.loss_fcn(prediction, label)
         return loss
 
-    def validation_step(self, batch,batch_idx):
+    def validation_step(self, batch, batch_idx):
         image,label = batch
-        prediction  = self.forward(image)
+        prediction = self.forward(image)
         loss = self.loss_fcn(prediction, label)
         return loss
 
@@ -47,7 +49,8 @@ class Linear(pl.LightningModule):
             nn.init.xavier_uniform_(m.weight.data)
         
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.5)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.config['MODEL']['learning_rate'])
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=self.config['MODEL']['lr_step_size'],
+                                                    gamma=self.config['MODEL']['lr_gamma'])
         return [optimizer], [scheduler]
 

Models/MixModel.py
@@ -1,82 +1,128 @@
 import matplotlib.pyplot as plt
 import torch
 import copy
+import pandas as pd
 from torch import nn
+import torch.distributed as dist
 from pytorch_lightning import LightningDataModule, LightningModule, Trainer, seed_everything
+from torchmetrics.classification import BinaryAccuracy, BinaryAUROC, BinaryF1Score
+from sklearn.metrics import roc_auc_score, f1_score, accuracy_score
+from torcheval.metrics.aggregation.auc import AUC
+from torcheval.metrics.toolkit import sync_and_compute
+
 from Losses.loss import WeightedMSE, CrossEntropy
 from sksurv.metrics import concordance_index_censored
 from monai.networks import nets
+
 
 class MixModel(LightningModule):
     def __init__(self, module_dict, config, loss_fcn=torch.nn.BCEWithLogitsLoss()):
         super().__init__()
         self.module_dict = module_dict
-        self.config      = config
-        self.loss_fcn    = getattr(torch.nn, self.config["MODEL"]["loss_function"])(pos_weight=torch.tensor(1.21))  # TODO: Why 1.21??, Doesn't work with CrossEntropyLoss
-        self.activation  = getattr(torch.nn, self.config["MODEL"]["activation"])()
-        self.classifier  = nn.Sequential(
-            nn.Linear(124, 120),
-            nn.Dropout(0.3),
+        self.config = config
+        # self.loss_fcn = getattr(torch.nn, self.config["MODEL"]["loss_function"])(pos_weight=torch.tensor(1.21))  # TODO: Why 1.21??, Doesn't work with CrossEntropyLoss
+        self.loss_fcn = getattr(torch.nn, self.config["MODEL"]["loss_function"])()
+        self.activation = getattr(torch.nn, self.config["MODEL"]["activation"])()
+        self.classifier = nn.Sequential(
+            nn.Linear(config['MODEL']['classifier_in'], 120),
+            nn.Dropout(config['MODEL']['dropout_prob']),
             nn.Linear(120, 40),
-            nn.Dropout(0.3),
+            nn.Dropout(config['MODEL']['dropout_prob']),
             nn.Linear(40, config['DATA']['n_classes']),
-            #self.activation
+            # self.activation
         )
         self.classifier.apply(self.weights_init)
 
+        self.train_accuracy = BinaryAccuracy()
+        self.train_auc = BinaryAUROC()
+        self.train_f1score = BinaryF1Score()
+        self.validation_accuracy = BinaryAccuracy()
+        self.validation_auc = BinaryAUROC()
+        self.validation_f1score = BinaryF1Score()
+
         self.training_outputs = []
-        self.validation_outputs = []
+        self.validation_outputs = [torch.empty((0, 2))]
 
     def forward(self, data_dict):
-        features   = torch.cat([self.module_dict[key](data_dict[key]) for key in self.module_dict.keys()
-                                if key in data_dict.keys()], dim=1)
+        features = torch.cat([self.module_dict[key](data_dict[key]) for key in self.module_dict.keys()
+                             if key in data_dict.keys()], dim=1)
         prediction = self.classifier(features)
         return prediction
 
     def training_step(self, batch, batch_idx):
         out = {}
-        data_dict, label = batch
+        data_dict, label = batch if 'censor_label' not in self.config['DATA'].keys() else (batch[0], batch[2])
         prediction = self.forward(data_dict)
         loss = self.loss_fcn(prediction.squeeze(dim=1), label)
         self.log("train_loss", loss, on_step=False, on_epoch=True, sync_dist=True)
+        self.train_accuracy(prediction.squeeze(dim=1), label)
+        self.train_auc(prediction.squeeze(dim=1), label)
+        self.train_f1score(prediction.squeeze(dim=1), label)
+        # self.log('train_acc_step', self.train_accuracy, sync_dist=True)
         MAE = torch.abs(prediction.flatten(0) - label)
         out['MAE'] = MAE.detach()
         out = copy.deepcopy(data_dict)
-        out['prediction'] = prediction.detach()
+        out['prediction'] = self.activation(prediction.detach())
         out['label'] = label
         out['loss'] = loss
-        self.training_outputs.append(out)
+        # train_results = torch.cat([self.activation(prediction.detach()), label[:, None]], dim=1)
+        # train_results_list = [torch.zeros_like(train_results) for _ in range(dist.get_world_size())]
+        # dist.all_gather(train_results_list, train_results)
+        # if len(self.training_outputs) > 0:
+        #     self.training_outputs[0] = torch.cat(
+        #         [self.training_outputs[0], torch.cat(train_results_list, dim=0)], dim=0)
+        # else:
+        #     self.training_outputs.append(torch.cat(train_results_list, dim=0))
         return out
 
     def on_train_epoch_end(self):
-        labels = torch.cat([out['label'] for i, out in enumerate(self.training_outputs)], dim=0)
-        prediction = torch.cat([out['prediction'] for i, out in enumerate(self.training_outputs)], dim=0)
+        # label = torch.cat([out['label'] for i, out in enumerate(self.training_outputs)], dim=0)
+        # prediction = torch.cat([out['prediction'] for i, out in enumerate(self.training_outputs)], dim=0)
+        # self.log("raw_train_results", torch.cat([labels[:, None], prediction], dim=1), sync_dist=True)
         # self.logger.report_epoch(prediction, labels, self.training_outputs,self.current_epoch, 'train_epoch_')
+        self.log('train_accuracy_epoch', self.train_accuracy, on_step=False, on_epoch=True, sync_dist=True,
+                 prog_bar=True)
+        self.log("train_auc_epoch", self.train_auc, on_step=False, on_epoch=True, sync_dist=True, prog_bar=False)
+        self.log('train_f1score_epoch', self.train_f1score, on_step=False, on_epoch=True, sync_dist=True,
+                 prog_bar=False)
+        # if self.global_rank == 0:
+        #     results = pd.DataFrame(self.training_outputs[0].cpu(), columns=['Prediction', 'Target'])
+        #     print(results)
+        #     print(accuracy_score(results['Target'].values, results['Prediction'].values >= 0.5))
+        #     # print(roc_auc_score(results['Target'], results['Prediction']))
+        #     print(f1_score(results['Target'].values, results['Prediction'].values >= 0.5, average='macro'))
         self.training_outputs.clear()
                                  
     def validation_step(self, batch, batch_idx):
         out = {}
-        data_dict, label = batch
+        data_dict, label = batch if 'censor_label' not in self.config['DATA'].keys() else (batch[0], batch[2])
         prediction = self.forward(data_dict)
         loss = self.loss_fcn(prediction.squeeze(dim=1), label)
         self.log("val_loss", loss, on_step=False, on_epoch=True, sync_dist=True)
+        self.validation_accuracy(prediction.squeeze(dim=1), label)
+        self.validation_auc(prediction.squeeze(dim=1), label)
+        self.validation_f1score(prediction.squeeze(dim=1), label)
         MAE = torch.abs(prediction.flatten(0) - label)
         out['MAE'] = MAE
         out = copy.deepcopy(data_dict)
-        out['prediction'] = prediction
+        out['prediction'] = prediction.detach()
         out['label'] = label
         out['loss'] = loss
-        self.validation_outputs.append(out)
         return out
 
     def on_validation_epoch_end(self):
-        labels = torch.cat([out['label'] for i, out in enumerate(self.validation_outputs)], dim=0)
-        prediction = torch.cat([out['prediction'] for i, out in enumerate(self.validation_outputs)], dim=0)
-        # self.logger.report_epoch(prediction.squeeze(), labels, self.validation_outputs, self.current_epoch,'val_epoch_')
+        # labels = torch.cat([out['label'] for i, out in enumerate(self.validation_outputs)], dim=0)
+        # prediction = torch.cat([out['prediction'] for i, out in enumerate(self.validation_outputs)], dim=0)
+        self.log('validation_accuracy_epoch', self.validation_accuracy, on_step=False, on_epoch=True,
+                 sync_dist=True, prog_bar=True)
+        self.log('validation_auc_epoch', self.validation_auc, on_step=False, on_epoch=True, sync_dist=True,
+                 prog_bar=False)
+        self.log('validation_f1score_epoch', self.validation_f1score, on_step=False, on_epoch=True,
+                 sync_dist=True, prog_bar=False)
         self.validation_outputs.clear()
 
     def test_step(self, batch, batch_idx):
-        data_dict, label = batch
+        data_dict, label = batch if 'censor_label' not in self.config['DATA'].keys() else (batch[0], batch[2])
         prediction = self.forward(data_dict)
         loss = self.loss_fcn(prediction.squeeze(dim=1), label)
         out = {}
@@ -97,6 +143,7 @@ class MixModel(LightningModule):
             m.reset_parameters()
             
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.5)
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.config['MODEL']['learning_rate'])
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=self.config['MODEL']['lr_step_size'],
+                                                    gamma=self.config['MODEL']['lr_gamma'])
         return [optimizer], [scheduler]

Utils/DataExtraction.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import numpy as np
-from prompt_toolkit import prompt
-from prompt_toolkit.completion import PathCompleter
-import os
 from pathlib import Path
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
 
 
 def create_subject_list(config):
@@ -12,17 +11,47 @@ def create_subject_list(config):
     data_columns = config['DATA']['clinical_cols'] + [config['DATA']['target']]
     if 'censor_label' in config['DATA'].keys():
         data_columns.append(config['DATA']['censor_label'])
+
     subject_list = pd.read_csv(config['DATA']['clinical_table_path'], index_col=config['DATA']['subject_label'])
     subject_list = subject_list.loc[patients, data_columns]
+
     # Certify event value is 0 and censored 1
     if 'censor_label' in config['DATA'].keys() and config['DATA']['event_value']:
         subject_list[config['DATA']['censor_label']] = (
             ~subject_list[config['DATA']['censor_label']].astype(bool)).astype(float)
         subject_list.rename({config['DATA']['censor_label']: 'Censored'}, axis=1, inplace=True)
         config['DATA']['censor_label'] = 'Censored'
+    elif 'censor_label' not in config['DATA'].keys():
+        subject_list[config['DATA']['censor_label']] = 1
+
     # Add each patient's modality paths
     for modality in config['MODALITY']:
         if config['MODALITY'][modality]:
             filename = config['DATA'][f'{modality}_path']
             subject_list[f'{modality}_Path'] = [pat / filename for pat in patient_paths]
+
+    # Exclude all patients with censored times before the threshold
+    exclusion_cond = ((subject_list.loc[:, config['DATA']['censor_label']] == 1) &
+                      (subject_list.loc[:, config['DATA']['target']] < config['DATA']['threshold']))
+    subject_list = subject_list.loc[~exclusion_cond]
+
+    if 'RECORDS' in config.keys() and config['RECORDS']['records'] and 'categorical_cols' in config['DATA']:
+        subject_list_old = subject_list.copy()
+        subject_list = pd.get_dummies(subject_list, columns=config['DATA']['categorical_cols'], drop_first=True,
+                                      dtype=np.float32)
+        new_categorical_cols = []
+        for var in config['DATA']['categorical_cols']:
+            cols = [c for c in list(subject_list.columns) if var in c]
+            subject_list.loc[subject_list_old[var].isna(), cols] = np.nan
+            new_categorical_cols += cols
+        config['DATA']['given_categorical_cols'] = config['DATA']['categorical_cols']
+        config['DATA']['given_clinical_cols'] = config['DATA']['clinical_cols']
+        config['DATA']['categorical_cols'] = new_categorical_cols
+        config['DATA']['clinical_cols'] = config['DATA']['continuous_cols'] + config['DATA']['categorical_cols']
+
+    if config['DATA']['imputation'] == 'mice':
+        imputer = IterativeImputer()
+        subject_list.loc[:, config['DATA']['clinical_cols']] = imputer.fit_transform(
+            subject_list.loc[:, config['DATA']['clinical_cols']]).astype(np.float32)
+
     return subject_list.reset_index()